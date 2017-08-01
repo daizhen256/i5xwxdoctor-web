d52f86ce46bfec7c2d3e420cec7a28d6ff3dcc2f
daizhen256 <daizhen256@sina.com>
2017-08-01 17:22:58 +0800

    改了标题

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 
 export default  {
-  name: 'dva-admin',
+  name: 'i5xwxdoctor-web',
   prefix: '',
-  footerText: 'dva-admin 版权所有 © 2016 由 pmg1989 提供支持',
-  logoSrc: 'https://o9u2lnvze.qnssl.com/web/global/brand.png',
-  logoText: 'DVA Admin',
-  needLogin: true
+  footerText: '天津市神州商龙科技有限公司 版权所有',
+  logoSrc: 'http://wx.qlogo.cn/mmopen/ajNVdqHZLLB1soficE1pC2uTql1MDqCqVqX0jo6pewchVJicXjQzj3V024hfia41tHRCfJJGoY9Pnu6DN1CJxehSg/0',
+  logoText: '智慧餐厅自检程序',
+  needLogin: false
 }
